d1209bb065cc1cd9478b3d56dc57d04ff9c1b236
Jaime <jaimequilez2601@yahoo.es>
2024-10-11 20:58:48 +0200

    Visualizar cuentas de calorias

---
+++ src/components/CalorieTracker.tsx
@@ -0,0 +1,61 @@
+import { useMemo } from "react";
+import type { Activity } from "../types";
+import CalorieDisplay from "./CalorieDisplay";
+
+type CalorieTrackerProps = {
+  activities: Activity[];
+};
+
+const CalorieTracker = ({ activities }: CalorieTrackerProps) => {
+  const caloriesConsumed = useMemo(
+    () =>
+      activities.reduce(
+        (total, activity) =>
+          activity.category === 1 ? total + activity.calories : total,
+        0
+      ),
+    [activities]
+  );
+
+  const caloriesBurned = useMemo(
+    () =>
+      activities.reduce(
+        (total, activity) =>
+          activity.category === 2 ? total + activity.calories : total,
+        0
+      ),
+    [activities]
+  );
+
+  const netCalories = useMemo(
+    () => caloriesConsumed - caloriesBurned,
+    [activities]
+  );
+
+  return (
+    <>
+      <h2 className="text-4xl font-black text-white text-center">
+        Resumen de calorias
+      </h2>
+      <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+        <CalorieDisplay
+          calories={caloriesConsumed}
+          text={"Consumidas"}
+          color={"text-orange-400"}
+        />
+        <CalorieDisplay
+          calories={caloriesBurned}
+          text={"Quemadas"}
+          color={"text-red-400"}
+        />
+        <CalorieDisplay
+          calories={netCalories}
+          text={"Diferencia"}
+          color={"text-white"}
+        />
+      </div>
+    </>
+  );
+};
+
+export default CalorieTracker;
